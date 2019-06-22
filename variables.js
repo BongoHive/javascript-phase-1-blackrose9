fe243b8a8034d15fe5ca612e9b87dd23315d666a
blackrose9 <sarah.mwendwa@outlook.com>
2019-06-22 14:52:49 +0200

    Deleted variable.html, edited variable.js
    Made some edits to variable.js to make it output to the console.

--- variables.js
+++ variables.js
@@ -1,33 +1,31 @@
 //javascript-phase-1-blackrose9
-function ageCalculator() { //declares a function
-    var thisYear = 2019; //this code declares the variable 'thisYear' and assigns the value '2019' to it.
-    var birthYear = 1996;
-    var futureYear = 2066;
-    
-    var possibleage1; //declares a variable
-    var possibleage2;
 
-    possibleage1 = futureYear - birthYear; //assigns value to variable
-    possibleage2 = futureYear - thisYear;
+//---------------------------Question1---------------------------------------//
+var birthYear = 1996; //this code declares the variable 'birthYear' and assigns the value '1996' to it.
+var futureYear = 2066;
 
-    console.log("First possible age is " + possibleage1); //outputs to the console
-    console.log("Second possible age is " + possibleage2);
+var possibleage1; //declares a variable 'possibleage1
+var possibleage2;
 
-}
+possibleage1 = futureYear - birthYear; //calculates my possible age in the future year according to my birth year. Eg. 21yrs
+possibleage2 = possibleage1 - 1; //calculates my second possible page within the future year. Eg. 20yrs before turning 21yrs
 
-function foodCalculator(){
-    var currentAge = 22; //declares a variable and assigns a value to it
-    var maxAge = 66;
-    var wingsPerDay = 24;
-    
-    //the code below declares a variable, assigns an expression which calculates a value
-    var wingsPerYear = wingsPerDay * 365; //multiplies wingsPerDay by 365 days in a year
+//outputs to the console
+console.log("Question 1");
+console.log("First possible age is " + possibleage1); 
+console.log("Second possible age is " + possibleage2);
 
-    var totalwingcount = (maxAge - currentAge) * wingsPerYear; //calculates the total wings eaten from now till maxAge
-    
-    //outputs to the console
-    console.log("You will need " + " " + totalwingcount + " " + "to last you until the ripe old age of" + " " + maxAge);
 
-    //outputs to HTML element with ID = 'outputq2'
-    document.getElementById("outputq2").innerHTML = ("You will need " + " " + totalwingcount + " " + "to last you until the ripe old age of" + " " + maxAge);
-}
+//-------------------------Question2-----------------------------------------//
+var currentAge = 22; //declares a variable 'currentAge' and assigns the value 22 to it
+var maxAge = 66;
+var wingsPerDay = 24;
+
+//the code below declares a variable, assigns an expression which calculates a value
+var wingsPerYear = wingsPerDay * 365; //multiplies wingsPerDay by 365 days in a year
+
+var totalwingcount = (maxAge - currentAge) * wingsPerYear; //calculates the total wings eaten from now till maxAge
+
+//outputs to the console
+console.log("Quesion 2");
+console.log("You will need " + " " + totalwingcount + " " + "to last you until the ripe old age of" + " " + maxAge);
